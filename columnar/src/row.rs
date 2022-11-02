abca5fa134ea317116cd41d36b3b4977fabeaad6
leeeon233 <leeeon233@gmail.com>
2022-11-02 21:41:40 +0800

    feat: add MapRow to auto encode HashMap

--- columnar/src/row.rs
+++ columnar/src/row.rs
@@ -1,14 +1,26 @@
-pub trait Row: Sized {
+use std::{borrow::Cow, collections::HashMap};
+
+use serde::{Deserialize, Serialize};
+
+pub trait VecRow: Sized {
     const FIELD_NUM: usize;
-    // fn types() -> Vec<TypeId>;
-    // fn get_attrs() -> Vec<ColumnAttr>;
-    // fn get_values<'a: 'c, 'c>(&'a self) -> Vec<Box<dyn Any>>;
-    // fn from_values(cells_data: Vec<Box<dyn Any>>) -> Result<Self, ColumnarError>;
-    fn serialize_vec_as_columns<S>(rows: &Vec<Self>, ser: S) -> Result<S::Ok, S::Error>
+    fn serialize_columns<S>(rows: &Vec<Self>, ser: S) -> Result<S::Ok, S::Error>
     where
         S: serde::Serializer;
 
-    fn deserialize_columns_to_vec<'de, D>(de: D) -> Result<Vec<Self>, D::Error>
+    fn deserialize_columns<'de, D>(de: D) -> Result<Vec<Self>, D::Error>
+    where
+        D: serde::Deserializer<'de>;
+}
+
+pub trait MapRow<'de>: Sized {
+    const FIELD_NUM: usize;
+    type Key: Serialize + Deserialize<'de>;
+    fn serialize_columns<S>(rows: &HashMap<Self::Key, Self>, ser: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer;
+
+    fn deserialize_columns<D>(de: D) -> Result<HashMap<Self::Key, Self>, D::Error>
     where
         D: serde::Deserializer<'de>;
 }
