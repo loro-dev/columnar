8494b9c3d1b4a8ceec518a1b0f866d35afa2d1a9
leeeon233 <leeeon233@gmail.com>
2023-05-31 22:12:24 +0800

    feat: compatible add wrapper support

--- columnar/examples/macro.rs
+++ columnar/examples/macro.rs
@@ -1,10 +1,25 @@
+use std::collections::HashMap;
+
+use serde::{Deserialize, Serialize};
 use serde_columnar::columnar;
 
-#[columnar(compatible, vec, ser, de)]
-#[derive(Debug)]
+#[columnar(map, vec, ser, de)]
+#[derive(Debug, Serialize, PartialEq, Deserialize, Clone)]
 struct Data {
     id: u64,
     s: String,
 }
+
+#[columnar(compatible, vec, ser, de)]
+#[derive(Debug)]
+struct Store {
+    id: u64,
+    s: String,
+    #[columnar(class = "vec")]
+    data: Vec<Data>,
+    #[columnar(class = "map")]
+    map: HashMap<String, Data>,
+}
+
 
 fn main() {}
--- columnar_derive/src/derive/serde.rs
+++ columnar_derive/src/derive/serde.rs
@@ -39,23 +39,41 @@ fn generate_per_element_ser(
     let mut start_optional = false;
     for args in field_args {
         let field_name = &args.ident;
+        let field_type = &args.ty;
         let optional = args.optional;
         let index = args.index;
+        // TODO: type vec map need wrapper
+        let class = &args.type_;
         if start_optional && !optional {
             return Err(syn::Error::new_spanned(
                 field_name,
                 "optional field must be placed after non-optional field",
             ));
         }
+
+        let after_wrapper_field = if let Some(type_) = class {
+            match type_.as_str() {
+                "vec" => {
+                    quote::quote!(&::serde_columnar::ColumnarVec::<_, #field_type>::new(&self.#field_name))
+                }
+                "map" => {
+                    quote::quote!(&::serde_columnar::ColumnarMap::<_, _, #field_type>::new(&self.#field_name))
+                }
+                _ => return Err(syn::Error::new_spanned(class, "unsupported type")),
+            }
+        } else {
+            quote::quote!(&self.#field_name)
+        };
+
         let e = if !optional {
             quote::quote!(
-                seq.serialize_element(&self.#field_name)?;
+                seq.serialize_element(#after_wrapper_field)?;
             )
         } else {
             start_optional = true;
             if let Some(index) = index {
                 quote::quote!(
-                    seq.serialize_element(&(#index, ::postcard::to_allocvec(&self.#field_name).map_err(S::Error::custom)?))?;
+                    seq.serialize_element(&(#index, ::postcard::to_allocvec(#after_wrapper_field).map_err(S::Error::custom)?))?;
                 )
             } else {
                 return Err(syn::Error::new_spanned(
@@ -132,6 +150,8 @@ fn generate_per_element_de(
         let field_name = &args.ident;
         let optional = args.optional;
         let index = args.index;
+        let field_type = &args.ty;
+        let class = &args.type_;
         if start_optional && !optional {
             return Err(syn::Error::new_spanned(
                 field_name,
@@ -139,9 +159,27 @@ fn generate_per_element_de(
             ));
         }
         let e = if !optional {
-            quote::quote!(
+            if let Some(type_) = class {
+                match type_.as_str() {
+                    "vec" => {
+                        quote::quote!(
+                            let wrapper: ::serde_columnar::ColumnarVec<_, #field_type> = seq.next_element()?.ok_or_else(|| A::Error::custom("DeserializeUnexpectedEnd"))?;
+                            let #field_name = wrapper.into_vec();
+                        )
+                    }
+                    "map" => {
+                        quote::quote!(
+                            let wrapper: ::serde_columnar::ColumnarMap<_, _, #field_type> = seq.next_element()?.ok_or_else(|| A::Error::custom("DeserializeUnexpectedEnd"))?;
+                            let #field_name = wrapper.into_map();
+                        )
+                    }
+                    _ => return Err(syn::Error::new_spanned(class, "unsupported type")),
+                }
+            } else {
+                quote::quote!(
                 let #field_name = seq.next_element()?.ok_or_else(|| A::Error::custom("DeserializeUnexpectedEnd"))?;
-            )
+                )
+            }
         } else {
             start_optional = true;
             if !add_mapping {
@@ -156,13 +194,39 @@ fn generate_per_element_de(
                 add_mapping = true;
             }
             if let Some(index) = index {
-                quote::quote!(
-                    let #field_name = if let Some(bytes) = mapping.remove(&#index){
-                        Some(::postcard::from_bytes(&bytes).map_err(A::Error::custom)?)
-                    }else{
-                        Default::default()
-                    };
-                )
+                if let Some(type_) = class {
+                    match type_.as_str() {
+                        "vec" => {
+                            quote::quote!(
+                                let #field_name = if let Some(bytes) = mapping.remove(&#index){
+                                    let wrapper: ::serde_columnar::ColumnarVec<_, #field_type> = ::postcard::from_bytes(&bytes).map_err(A::Error::custom)?;
+                                    wrapper.into_vec()
+                                }else{
+                                    Default::default()
+                                };
+                            )
+                        }
+                        "map" => {
+                            quote::quote!(
+                                let #field_name = if let Some(bytes) = mapping.remove(&#index){
+                                    let wrapper: ::serde_columnar::ColumnarMap<_, _, #field_type> = ::postcard::from_bytes(&bytes).map_err(A::Error::custom)?;
+                                    wrapper.into_map()
+                                }else{
+                                    Default::default()
+                                };
+                            )
+                        }
+                        _ => return Err(syn::Error::new_spanned(class, "unsupported type")),
+                    }
+                } else {
+                    quote::quote!(
+                        let #field_name = if let Some(bytes) = mapping.remove(&#index){
+                            ::postcard::from_bytes(&bytes).map_err(A::Error::custom)?
+                        }else{
+                            Default::default()
+                        };
+                    )
+                }
             } else {
                 return Err(syn::Error::new_spanned(
                     field_name,
